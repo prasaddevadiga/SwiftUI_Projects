13e5340247341ed9a2e54fb2a5900f6f2c135d6e
Prasad Devadiga <prasad>
2021-06-05 20:45:29 +0530

    Added badge to tabbar item

--- MenuList/MenuList/Views/MainView.swift
+++ MenuList/MenuList/Views/MainView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject var order: Order
+    
     var body: some View {
-        TabView {
-            ContentView()
-                .tabItem {
-                    Label("Menu", systemImage: "list.dash")
+        
+        GeometryReader { geometry in
+            ZStack(alignment: .bottomLeading) {
+                TabView {
+                    ContentView()
+                        .tabItem {
+                            Label("Menu", systemImage: "list.dash")
+                        }.tag(0)
+                    OrderView()
+                        .tabItem {
+                            Label("Order", systemImage: "square.and.pencil")
+                        }.tag(1)
                 }
-            OrderView()
-                .tabItem {
-                    Label("Order \(order.items.count)", systemImage: "square.and.pencil")
+                
+                ZStack {
+                    Circle()
+                        .foregroundColor(.red)
+
+                        Text("\(self.order.items.count)")
+                        .foregroundColor(.white)
+                        .font(Font.system(size: 12))
                 }
+                .frame(width: 15, height: 15)
+                .offset(x: 3.0 * ( geometry.size.width / 4 ) + 2, y: -30)
+                .opacity(self.order.items.count == 0 ? 0 : 1.0)
+            }
         }
     }
 }
